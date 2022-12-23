a40a1e908ba05a463aa114374fbb1cdd060f0b89
Miquel Angel <hulkrojo2@gmail.com>
2022-12-23 12:40:26 +0100

    refactor(project): Renamed all project to me.pceconomic.project on the java project

--- back/esquelet-BotigaInformatica/src/main/java/me/pceconomic/shop/EsqueletBotigaInformaticaApplication.java
+++ back/esquelet-BotigaInformatica/src/main/java/me/pceconomic/shop/EsqueletBotigaInformaticaApplication.java
@@ -1,4 +1,4 @@
-package cat.iesmanacor.projecte.esqueletbotigainformatica;
+package me.pceconomic.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
