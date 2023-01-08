5e2d23cf689c1b6c8defe30788399b81975228fe
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-01-08 01:01:56 +0100

    canvis amb git ignore

--- public/esquelet-BotigaInformatica/src/main/java/me/pceconomic/shop/domain/entities/persona/Direccio.java
+++ public/esquelet-BotigaInformatica/src/main/java/me/pceconomic/shop/domain/entities/persona/Direccio.java
@@ -1,2 +1,32 @@
-package me.pceconomic.shop.domain.entities.persona;public class Direccio {
+package me.pceconomic.shop.domain.entities.persona;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "direccions")
+public class Direccio {
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "carrer")
+    private String street;
+
+    @Column(name = "numero")
+    private int number;
+
+    @Column(name = "poblacio")
+    private String city;
+
+    @Column(name = "provincia")
+    private String province;
+
+    @Column(name = "codiPostal")
+    private String postalCode;
+
+    @Column(name = "pais")
+    private String country;
+
+    @Column(name = "estaActiva")
+    private boolean isActive;
 }
