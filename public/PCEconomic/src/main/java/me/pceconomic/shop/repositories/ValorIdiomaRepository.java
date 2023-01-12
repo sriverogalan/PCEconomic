9c1670596b9a1ad7c5c415b268bd9a3ba2102903
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-01-12 20:25:27 +0100

    implements bootstrap

--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ValorIdiomaRepository.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ValorIdiomaRepository.java
@@ -1,7 +1,8 @@
 package me.pceconomic.shop.repositories;
 
+import me.pceconomic.shop.domain.entities.idiomes.ValorIdioma;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ValorIdiomaRepository extends JpaRepository<ValorIdiomaRepository, Integer> {
+public interface ValorIdiomaRepository extends JpaRepository<ValorIdioma, Integer> {
 
 }
