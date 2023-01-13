d2821f643216f0d7001d9e45ea7dc210431dcf4b
Miquel Angel <hulkrojo2@gmail.com>
2023-01-13 23:45:34 +0100

    feat(controller): added issuecontroller that will redirect the user to a error 404

---
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/IssueController.java
@@ -0,0 +1,14 @@
+package me.pceconomic.shop.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IssueController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
