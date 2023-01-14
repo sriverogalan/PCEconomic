64c63f33221431c406275d578b82abf45e9613fc
Miquel Angel <hulkrojo2@gmail.com>
2023-01-14 17:56:47 +0100

    feat(controller): added possibility to view the categories and its assigned articles

--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/CategoriaRepository.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/CategoriaRepository.java
@@ -4,4 +4,7 @@ import me.pceconomic.shop.domain.entities.article.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    Categoria findByName(String name);
+
 }
