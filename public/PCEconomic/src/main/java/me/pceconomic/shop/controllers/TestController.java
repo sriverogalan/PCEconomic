a8ba25beda1871e4a677e00d3b142df1ee905f24
Miquel Angel <hulkrojo2@gmail.com>
2023-01-16 19:13:54 +0100

    canvis

---
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/TestController.java
@@ -0,0 +1,49 @@
+package me.pceconomic.shop.controllers;
+
+import me.pceconomic.shop.domain.entities.article.propietats.Propietat;
+import me.pceconomic.shop.domain.entities.article.propietats.Propietats;
+import me.pceconomic.shop.domain.entities.article.propietats.PropietatsValor;
+import me.pceconomic.shop.domain.entities.article.propietats.Valor;
+import me.pceconomic.shop.repositories.PropietatRepository;
+import me.pceconomic.shop.repositories.PropietatsRepository;
+import me.pceconomic.shop.repositories.PropietatsValorRepository;
+import me.pceconomic.shop.repositories.ValorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private PropietatRepository propietatRepository;
+    @Autowired
+    private PropietatsRepository propietatsRepository;
+    @Autowired
+    private ValorRepository valorRepository;
+    @Autowired
+    private PropietatsValorRepository propietatsValorRepository;
+
+    @GetMapping("/test/propietats")
+    public List<Propietat> getValors() {
+        return propietatRepository.findAll();
+    }
+
+    @GetMapping("/test/valors")
+    public List<Valor> getProp() {
+        return valorRepository.findAll();
+    }
+
+    @GetMapping("/test/propietatsvalors")
+    public List<PropietatsValor> getPropValors() {
+        return propietatsValorRepository.findAll();
+    }
+
+    @GetMapping("/test/propietatsssss")
+    public List<Propietats> aaaaaa() {
+        return propietatsRepository.findAll();
+    }
+
+}
