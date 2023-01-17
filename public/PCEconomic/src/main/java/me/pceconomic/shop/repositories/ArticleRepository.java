6ce463a95a964954e26cbc2d9aa4e18d0d35b403
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-01-17 18:28:53 +0100

    fix(article) v0.0.8-snapshot -

--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ArticleRepository.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ArticleRepository.java
@@ -2,13 +2,14 @@ package me.pceconomic.shop.repositories;
 
 import me.pceconomic.shop.domain.entities.article.Article;
 import me.pceconomic.shop.domain.entities.article.categoria.Categoria;
+import me.pceconomic.shop.domain.entities.article.categoria.Subcategoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Set;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
 
-    Set<Article> findByCategories(Categoria categoria);
+    Set<Article> findBySubcategories(Subcategoria subcategoria);
 
 
 
