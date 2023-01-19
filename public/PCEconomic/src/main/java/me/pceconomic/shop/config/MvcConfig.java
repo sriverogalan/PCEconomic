8e447f3222ccff1ebbb9971af24af7c0b2c69c71
Miquel Angel <hulkrojo2@gmail.com>
2023-01-19 11:37:35 +0100

    refactor: cleaned up MvcConfig

--- public/PCEconomic/src/main/java/me/pceconomic/shop/config/MvcConfig.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/config/MvcConfig.java
@@ -9,8 +9,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/areaclients").setViewName("areaclients");
     }
 
 }
