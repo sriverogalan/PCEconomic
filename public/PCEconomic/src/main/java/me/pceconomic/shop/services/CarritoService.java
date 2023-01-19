4ee420fc5eef30cc13b5175c3e214a8e1830deb8
Miquel Angel <hulkrojo2@gmail.com>
2023-01-19 11:38:01 +0100

    feat: added simple method on cartservice

--- public/PCEconomic/src/main/java/me/pceconomic/shop/services/CarritoService.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/services/CarritoService.java
@@ -1,8 +1,12 @@
 package me.pceconomic.shop.services;
 
 import jakarta.servlet.http.HttpSession;
+import me.pceconomic.shop.domain.entities.article.propietats.Propietats;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CarritoService {
@@ -14,5 +18,12 @@ public class CarritoService {
         this.session = session;
     }
 
-
+    public List<Propietats> getCarrito() {
+        List<Propietats> carrito = (List<Propietats>) session.getAttribute("carrito");
+        if (carrito == null) {
+            carrito = new ArrayList<>();
+            session.setAttribute("carrito", carrito);
+        }
+        return carrito;
+    }
 }
