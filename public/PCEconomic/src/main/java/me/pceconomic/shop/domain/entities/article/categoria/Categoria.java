d5818c3aa40ce74cfb709e66e73bf4b3dcedccb7
Sergi Rivero Galán <56429173+sriverogalan@users.noreply.github.com>
2023-01-19 11:39:27 +0100

    Changes

--- public/PCEconomic/src/main/java/me/pceconomic/shop/domain/entities/article/categoria/Categoria.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/domain/entities/article/categoria/Categoria.java
@@ -1,5 +1,6 @@
 package me.pceconomic.shop.domain.entities.article.categoria;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
