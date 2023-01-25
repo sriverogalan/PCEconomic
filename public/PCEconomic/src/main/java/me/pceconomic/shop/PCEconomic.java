7aacbad8ae18b19a9e00bd9927346e0e6a4d29f9
Miquel Angel <hulkrojo2@gmail.com>
2023-01-25 13:30:10 +0100

    refactor: changed main class to PCEconomic

---
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/PCEconomic.java
@@ -0,0 +1,14 @@
+package me.pceconomic.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PCEconomic extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PCEconomic.class, args);
+    }
+
+}
