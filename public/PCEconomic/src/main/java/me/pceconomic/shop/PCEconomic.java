0eae0f4de4adfbcbdc60701c4a96f89a7f87b151
Miquel Angel <hulkrojo2@gmail.com>
2023-01-26 22:30:02 +0100

    refactor: changed main class name to PCEconomic

--- public/PCEconomic/src/main/java/me/pceconomic/shop/PCEconomic.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/PCEconomic.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EsqueletBotigaInformaticaApplication extends SpringBootServletInitializer {
+public class PCEconomic extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(EsqueletBotigaInformaticaApplication.class, args);
+        SpringApplication.run(PCEconomic.class, args);
     }
 
 }
