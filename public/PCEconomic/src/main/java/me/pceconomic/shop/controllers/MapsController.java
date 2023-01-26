708998bee8291bf490472da58f5c46ebcb813c92
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-01-26 23:15:57 +0100

    changed

--- public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/MapsController.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/MapsController.java
@@ -1,7 +1,10 @@
 package me.pceconomic.shop.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -11,17 +14,14 @@ import java.net.URL;
 
 @RestController
 public class MapsController {
+    @Value("${google.maps.key}")
+    private String key;
     @GetMapping("/maps")
-    public String getDistance(HttpServletRequest request) throws Exception {
-        String origin = request.getParameter("origin");
-        String destination = request.getParameter("destination");
-        String key = "AIzaSyA7sDeOoFe2KTvrHeePf3zn43NMrGzVzXQ";
-        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Manacor&destinations=Inca,%20Mallorca,%20Spain&units=imperial&key=" + key;
+    public String getDistance() throws Exception {
+        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Manacor&destinations=Inca,%20Mallorca,%20Spain&units=imperial&key=" + this.key;
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
-
-        // devuelve un json
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
