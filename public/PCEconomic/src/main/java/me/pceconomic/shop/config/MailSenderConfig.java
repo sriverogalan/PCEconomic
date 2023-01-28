d73ce90a1242b73619b64a26bef9694e95894265
Miquel Angel <hulkrojo2@gmail.com>
2023-01-28 17:15:19 +0100

    feat: turned debug off for MailConfig

--- public/PCEconomic/src/main/java/me/pceconomic/shop/config/MailSenderConfig.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/config/MailSenderConfig.java
@@ -26,7 +26,7 @@ public class MailSenderConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false");
 
         return mailSender;
     }
