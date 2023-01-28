36ed258e9dea25c4e1d40a2692492ac8e2c75c51
Miquel Angel <hulkrojo2@gmail.com>
2023-01-28 17:15:46 +0100

    feat: added MailService with a default from mail

--- public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/TestController.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/TestController.java
@@ -1,25 +1,23 @@
 package me.pceconomic.shop.controllers;
 
+import me.pceconomic.shop.services.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final MailService mailService;
+
     @Autowired
-    private JavaMailSender javaMailSender;
+    public TestController(MailService mailService) {
+        this.mailService = mailService;
+    }
 
     @GetMapping("/sendMail")
     public String sendMail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("miquelangel2309@gmail.com");
-        message.setTo("hulkrojo2@gmail.com");
-        message.setSubject("Testing");
-        message.setText("This is for testing purposes");
-        javaMailSender.send(message);
+        mailService.sendMail("hulkrojo2@gmail.com", "Test", "Test");
         return "Mail sent";
     }
 
