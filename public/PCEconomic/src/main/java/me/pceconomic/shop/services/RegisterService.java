63d251980307cf7569b872ffadb44c25f7889df7
Miquel Angel <hulkrojo2@gmail.com>
2023-01-28 17:51:28 +0100

    fix: still fixing warnings

--- public/PCEconomic/src/main/java/me/pceconomic/shop/services/RegisterService.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/services/RegisterService.java
@@ -8,8 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.regex.Pattern;
-
 @Service
 public class RegisterService {
 
