904aa2aca6e625cc657b993f6e1d1bb97fa721de
Miquel Angel <hulkrojo2@gmail.com>
2023-02-02 17:06:15 +0100

    refactor: changed all directions to be all in one table to be more easy to interact with

--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ComunitatAutonomaRepository.java
+++
@@ -1,8 +0,0 @@
-package me.pceconomic.shop.repositories;
-
-import me.pceconomic.shop.domain.entities.persona.direction.ComunitatAutonoma;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComunitatAutonomaRepository extends JpaRepository<ComunitatAutonoma, Integer> {
-
-}
--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ProvinciaRepository.java
+++
@@ -1,8 +0,0 @@
-package me.pceconomic.shop.repositories;
-
-import me.pceconomic.shop.domain.entities.persona.direction.Provincia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProvinciaRepository extends JpaRepository<Provincia, Integer> {
-
-}
