7472d99af2bc13263ccb3cd6ba2aaf316ff5648d
Miquel Angel <hulkrojo2@gmail.com>
2023-02-03 20:17:50 +0100

    feat: fixed some issues with thymeleaf doesnt recognize objects

--- public/PCEconomic/src/main/java/me/pceconomic/shop/services/AreaClientsService.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/services/AreaClientsService.java
@@ -1,29 +1,43 @@
 package me.pceconomic.shop.services;
 
 
+import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
 import me.pceconomic.shop.domain.entities.persona.Client;
 import me.pceconomic.shop.domain.entities.persona.Direccio;
 import me.pceconomic.shop.domain.entities.persona.Persona;
 import me.pceconomic.shop.domain.forms.AddDirectionForm;
 import me.pceconomic.shop.domain.forms.ChangeNameForm;
+import me.pceconomic.shop.domain.forms.ChangePasswordForm;
 import me.pceconomic.shop.repositories.ClientRepository;
 import me.pceconomic.shop.repositories.DireccioRepository;
 import me.pceconomic.shop.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
-@Service
+@Service @Getter
 public class AreaClientsService {
 
     private final ClientRepository clientRepository;
     private final PersonaRepository personaRepository;
     private final DireccioRepository direccioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AreaClientsService(PersonaRepository personaRepository, DireccioRepository direccioRepository, ClientRepository clientRepository) {
+    public AreaClientsService(PasswordEncoder passwordEncoder, PersonaRepository personaRepository, DireccioRepository direccioRepository, ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
         this.personaRepository = personaRepository;
         this.direccioRepository = direccioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void sendToModel(Model model, HttpSession session) {
+        model.addAttribute("user", session.getAttribute("persona"));
+        model.addAttribute("changeName", new ChangeNameForm());
+        model.addAttribute("directionForm", new AddDirectionForm());
+        model.addAttribute("changePasswordForm", new ChangePasswordForm());
     }
 
     public void saveDirection(Client client, AddDirectionForm directionForm) {
@@ -43,5 +57,16 @@ public class AreaClientsService {
         persona.setName(changeNameForm.getNewName());
         personaRepository.save(persona);
     }
+
+    public void changePassword(Client client, ChangePasswordForm changePasswordForm) {
+        Persona persona = client.getPersona();
+
+        String encodedPassword = passwordEncoder.encode(changePasswordForm.getNewPassword());
+
+        persona.setPassword(encodedPassword);
+        personaRepository.save(persona);
+    }
+
+
 
 }
