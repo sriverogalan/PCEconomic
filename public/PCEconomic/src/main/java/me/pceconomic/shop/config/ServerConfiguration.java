a55f48ac946842e52bc1b593e37d8dfb71a4cee6
Miquel Angel <hulkrojo2@gmail.com>
2023-02-03 10:49:20 +0100

    fix: a better fix for static files 404

--- public/PCEconomic/src/main/java/me/pceconomic/shop/config/ServerConfiguration.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/config/ServerConfiguration.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
+@EnableWebMvc
 public class ServerConfiguration implements WebMvcConfigurer {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
     private final LoginInterceptor loginInterceptor;
 
     @Autowired
@@ -25,6 +29,12 @@ public class ServerConfiguration implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedOrigins("http://localhost:80", "http://localhost:5500", "http://localhost:8080")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
     @Override
