b477e86f9ce925571c172659f27a5c76253c56d0
Miquel Angel <hulkrojo2@gmail.com>
2023-02-03 20:18:17 +0100

    feat: endpoint to encode passwords

--- public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/TestController.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/TestController.java
@@ -8,7 +8,9 @@ import me.pceconomic.shop.domain.entities.persona.Persona;
 import me.pceconomic.shop.repositories.ClientRepository;
 import me.pceconomic.shop.repositories.PersonaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,11 +21,13 @@ public class TestController {
 
     private final PersonaRepository personaRepository;
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public TestController(PersonaRepository personaRepository, ClientRepository clientRepository) {
+    public TestController(PasswordEncoder passwordEncoder, PersonaRepository personaRepository, ClientRepository clientRepository) {
         this.personaRepository = personaRepository;
         this.clientRepository = clientRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/api/persones")
@@ -34,6 +38,11 @@ public class TestController {
     @GetMapping("/api/clients")
     public List<Client> test2() {
         return clientRepository.findAll();
+    }
+
+    @GetMapping("/encode")
+    public String encode(@RequestParam String p) {
+        return passwordEncoder.encode(p);
     }
 
 }
