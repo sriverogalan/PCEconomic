3b1c1577e324856bd1f2a9b1df442596a5cd832c
Sergi Rivero <56429173+sriverogalan@users.noreply.github.com>
2023-02-20 18:09:04 +0100

    fix(article) v0.0.3 new IdiomaBase class and new Idioma class

--- public/PCEconomic/src/main/java/me/pceconomic/shop/repositories/ClauRepository.java
+++
@@ -1,8 +0,0 @@
-package me.pceconomic.shop.repositories;
-
-import me.pceconomic.shop.domain.entities.idiomes.Clau;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClauRepository extends JpaRepository<Clau, Integer> {
-
-}
