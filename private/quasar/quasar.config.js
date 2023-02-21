6372a88f9db5bc12ba93db1a3b6731d5707a6b83
Sergi Rivero Galán <sriverogalan@gmail.com>
2023-02-21 11:24:19 +0100

    Quasar

--- private/quasar/quasar.config.js
+++ private/quasar/quasar.config.js
@@ -49,6 +49,13 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      extendWebpack(cfg) {
+        cfg.watchOptions = {
+          aggregateTimeout: 200,
+          poll: 1000,
+        };
+      },
+      
 
       // transpile: false,
       // publicPath: '/',
