dd69e47b732c7d3dd0a788cce8b729915a63991d
Miquel Angel <hulkrojo2@gmail.com>
2023-02-21 14:50:25 +0100

    fix: fixed token rest controller retuning errors

--- public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/rest/RestTokenController.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/rest/RestTokenController.java
@@ -1,6 +1,7 @@
 package me.pceconomic.shop.controllers.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,12 @@ public class RestTokenController {
 
     @GetMapping("/token")
     public String getToken(HttpServletRequest request) {
-        return request.getSession().getAttribute("token").toString();
+        HttpSession session = request.getSession();
+        if (session == null) return "No Session Found";
+
+        String token = (String) session.getAttribute("token");
+        if (token == null) return "No Token Found";
+
+        return token;
     }
 }
