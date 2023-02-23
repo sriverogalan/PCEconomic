d02950c5ac4da67931dbf1a3c9d87b31f08a3b1b
Sergi Rivero Galán <sriverogalan@gmail.com>
2023-02-23 13:55:34 +0100

    changed indexPage.vue

--- private/quasar/src/pages/IndexPage.vue
+++ private/quasar/src/pages/IndexPage.vue
@@ -3,33 +3,86 @@
     <div class="col-10">
       <h1 class="col-12 text-center">Gestiona tus usuarios</h1>
       <div class="q-pa-md">
-        <q-table title="Usuaris" :rows="rows" :columns="columns" row-key="name" />
+        <q-table
+          title="Usuarios"
+          :rows="rowsFiltrats"
+          :columns="columns"
+          row-key="nom"
+          no-data="no hay na"
+        /> 
       </div>
     </div>
   </q-page>
 </template>
 
 <script>
+import axios from "axios";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
   data() {
     return {
-      columns: [],
-      columnsFiltrades: [],
+      columns: [
+        {
+          name: "Nom",
+          required: true,
+          label: "Nom",
+          align: "center",
+          field: (row) => row.nom, 
+          sortable: true,
+        },
+        {
+          name: "cognoms",
+          align: "center",
+          label: "Cognoms",
+          field: "cognoms",
+          sortable: true,
+        },
+        {
+          name: "email",
+          align: "center",
+          label: "Email",
+          field: "email",
+          sortable: true,
+        },
+        {
+          name: "telefon",
+          align: "center",
+          label: "Telefon",
+          field: "telefon",
+          sortable: true,
+        },
+        {
+          name: "actions",
+          align: "center",
+          label: "Acciones",
+          field: "actions",
+        },
+      ],
       rows: [],
-      rowsFiltrades: [],
+      rowsFiltrats: [],
     };
   },
   methods: {
-
-
+    async getPersones() {
+      const personesAxios = await axios.get("http://localhost:8000/persones");
+      const personesJson = await personesAxios.data;
+      console.log(personesJson);
+      personesJson.map((p) => {
+        this.rows.push({
+          nom: p.nom,
+          cognoms: p.cognom1 + " " + p.cognom2,
+          email: p.email,
+          telefon: p.telefon,
+        });
+        console.log(this.rows);
+      });
+      this.rowsFiltrats = this.rows;
+    },
   },
-
-  setup() {
-
-    
+  mounted() {
+    this.getPersones();
   },
 });
 </script>
