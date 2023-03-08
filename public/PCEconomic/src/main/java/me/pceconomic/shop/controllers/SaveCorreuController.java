653bd8b04b5a02f7d7a613ea8e512651d12274ef
Miquel Angel <hulkrojo2@gmail.com>
2023-03-08 10:46:41 +0100

    feat: store email when stock is 0

--- public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/SaveCorreuController.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/controllers/SaveCorreuController.java
@@ -1,2 +1,43 @@
-package me.pceconomic.shop.controllers;public class SaveCorreuController {
+package me.pceconomic.shop.controllers;
+
+import me.pceconomic.shop.domain.entities.article.propietats.Propietats;
+import me.pceconomic.shop.domain.entities.persona.CorreoNoStock;
+import me.pceconomic.shop.repositories.CorreoNoStockRepository;
+import me.pceconomic.shop.repositories.PropietatsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SaveCorreuController {
+
+    private final CorreoNoStockRepository correoNoStockRepository;
+    private final PropietatsRepository propietatsRepository;
+    @Autowired
+    public SaveCorreuController(CorreoNoStockRepository correoNoStockRepository, PropietatsRepository propietatsRepository) {
+        this.correoNoStockRepository = correoNoStockRepository;
+        this.propietatsRepository = propietatsRepository;
+    }
+
+    @PostMapping("/savecorreu")
+    public ResponseEntity<String> saveCorreu(@RequestParam String correu, @RequestParam String id) {
+        Propietats propietats = propietatsRepository.findById(Integer.parseInt(id)).orElse(null);
+
+        if (propietats == null) {
+            return new ResponseEntity<>("Error al guardar el correo", HttpStatus.BAD_REQUEST);
+        }
+
+        if (correoNoStockRepository.existsByEmail(correu)) {
+            return new ResponseEntity<>("ALREDY_EXISTS", HttpStatus.BAD_REQUEST);
+        }
+
+        CorreoNoStock correoNoStock = new CorreoNoStock();
+        correoNoStock.setEmail(correu);
+        correoNoStock.setPropietats(propietats);
+
+        correoNoStockRepository.save(correoNoStock);
+
+        return new ResponseEntity<>("Correo guardado correctamente", HttpStatus.OK);
+    }
 }
--- public/PCEconomic/src/main/java/me/pceconomic/shop/domain/entities/persona/CorreoNoStock.java
+++ public/PCEconomic/src/main/java/me/pceconomic/shop/domain/entities/persona/CorreoNoStock.java
@@ -1,2 +1,20 @@
-package me.pceconomic.shop.domain.entities.persona;public class CorreoNoStockl {
+package me.pceconomic.shop.domain.entities.persona;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import me.pceconomic.shop.domain.entities.article.propietats.Propietats;
+
+@Entity(name = "correos_usuarios_no_stock")
+public @Data class CorreoNoStock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_correo")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "id_propietats")
+    private Propietats propietats;
 }
