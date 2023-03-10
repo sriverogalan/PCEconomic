5896d4ef7b1467c4fe9bd1617926f09111069b07
Sergi Rivero Galán <sriverogalan@gmail.com>
2023-03-10 13:39:41 +0100

    changed axios.js

--- private/quasar/src/boot/axios.js
+++ private/quasar/src/boot/axios.js
@@ -19,8 +19,8 @@ export default boot(({ app }) => {
   axios.interceptors.request.use(function (config) {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-      config.headers["Content-Type"] = "application/json";
+      config.headers.Authorization = `Bearer ${token}`;/* 
+      config.headers["Content-Type"] = "application/json"; */
     }
     return config;
   });
